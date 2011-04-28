f1ef8ee3b7dff3eb16205daa189f27d539c2f6c4
Francisco Souza <francisco@franciscosouza.net>
2011-04-27 23:17:47 -0300

    Starting GAE server before running tests and stopping it after running tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,39 @@
+import os
+import subprocess
+from urllib import urlopen
+
+APP_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+BASE_URL = "http://localhost:8080/"
+get_url = lambda path: "%s%s" % (BASE_URL, path.lstrip("/"))
+
 class Environment(object):
     """Dummy class for data storage"""
     pass
 
 env = Environment()
 
-get_url = lambda path: "http://localhost:8080/%s" % path.lstrip("/")
+def wait_until_server_start():
+    while True:
+        try:
+            urlopen(BASE_URL)
+            break
+        except IOError:
+            pass
+
+def wait_until_server_stop():
+    while True:
+        try:
+            response = urlopen(BASE_URL)
+            if response.code == 404:
+                break
+        except IOError:
+            break
+
+def setup():
+    output = open('/dev/null', 'w')
+    env.server_process = subprocess.Popen(['python2.5', '/usr/local/google_appengine/dev_appserver.py', '--clear_datastore', APP_PATH], stdout=output, stderr=output)
+    wait_until_server_start()
+
+def teardown(total):
+    subprocess.call(['kill' , '-9', '%d' % env.server_process.pid])
+    wait_until_server_stop()
